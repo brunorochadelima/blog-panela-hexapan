8699d57a26fae47ee34042b550d7eed264ee90dd
Bruno Rocha de Lima <brunorocha33@live.com>
2022-05-02 13:44:03 -0300

    página 404

--- src/pages/Pagina404.tsx
+++ src/pages/Pagina404.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import imagem404 from "assets/img-404.png";
+import styles from "pages/Pagina404.module.scss";
+import Header from "components/Header";
+import { Link } from "react-router-dom";
 
 export default function Pagina404() {
   return (
-    <div>
-      <h1>OOOPS!!!</h1>
-      <img src={imagem404} alt="Erro 404" />
-      <p>Não encontramos esta página</p>
-    </div>
+    <>
+      <Header />
+      <div className={styles.pagina_404}>
+        <div>
+          <h1>OOOPS!!!</h1>
+          <img src={imagem404} alt="Erro 404" />
+          <p>Não encontramos esta página! 😞</p>
+          <Link to="/" className={styles.botao__voltar}>
+            <button>Quero Voltara para a página inicial</button>
+          </Link>
+        </div>
+      </div>
+    </>
   );
 }
