4e7dcf30a34fdbf58ad71e03b699c4d63159685d
Bruno Rocha de Lima <brunorocha33@live.com>
2022-05-05 10:51:30 -0300

    ajustando detalhes footer e header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,29 +10,31 @@ export default function Footer() {
     <div>
       <div className={styles.container__footer}>
         <img src={logo} alt="Logo Hexapan" />
-        <a
-          href="https://api.whatsapp.com/send?phone=553484070716&text=Ola%20Multivisi"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <AiOutlineWhatsApp className={styles.icon_color} size={33} />
-        </a>
-        <a
-          href="https://www.facebook.com/hexapanbrasil/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {' '}
-          <AiOutlineFacebook className={styles.icon_color} size={33} />
-        </a>
-        <a
-          href="https://www.instagram.com/hexapanbrasil/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {' '}
-          <AiOutlineInstagram className={styles.icon_color} size={33} />
-        </a>
+        <div>
+          <a
+            href="https://api.whatsapp.com/send?phone=553484070716&text=Ola%20Multivisi"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiOutlineWhatsApp className={styles.icon_color} size={33} />
+          </a>
+          <a
+            href="https://www.facebook.com/hexapanbrasil/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {' '}
+            <AiOutlineFacebook className={styles.icon_color} size={33} />
+          </a>
+          <a
+            href="https://www.instagram.com/hexapanbrasil/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {' '}
+            <AiOutlineInstagram className={styles.icon_color} size={33} />
+          </a>
+        </div>
       </div>
       <p className={styles.copyright}>
         A Hexapan pertence ao Grupo Multivisi. © Copyright 2022. Todos os
