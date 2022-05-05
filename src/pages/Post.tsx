6e578fc26caad0a9d87b2a05d6e3abd4a9674004
Bruno Rocha de Lima <brunorocha33@live.com>
2022-05-05 10:50:31 -0300

    criando post

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import cards from 'data/cards.json';
+import { useNavigate } from 'react-router-dom';
+import styles from './Post.module.scss';
+import stylesTema from 'styles/Tema.module.scss';
 
 export default function Post() {
   const { id } = useParams();
@@ -9,10 +12,20 @@ export default function Post() {
     return 'null';
   }
 
+  const navigate = useNavigate();
+
   return (
-    <section>
+    <section className={stylesTema.container}>
       <article>
-        <h1>{post.title}</h1>
+        <h1 className={styles.post__titulo}>{post.title}</h1>
+        <div className={styles.post__img__container}>
+          <img className={styles.post__img} src={post.photo} alt={post.title} />
+        </div>
+        <p className={styles.post__data}>{post.date}</p>
+        <p className={styles.post__descricao}>{post.description}</p>
+        <button className={styles.post__voltar} onClick={() => navigate(-1)}>
+          {'< Voltar'}
+        </button>
       </article>
     </section>
   );
