775ed5dec05f9240206f6936ac8cfd87cdd65b94
Bruno Rocha de Lima <brunorocha33@live.com>
2022-05-09 08:08:33 -0300

    Identando código

--- src/routes.js
+++ src/routes.js
@@ -14,7 +14,7 @@ export default function AppRouter() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<Pagina404 />} />
-          <Route path="post/:id" element={<Post/>} />
+          <Route path="post/:id" element={<Post />} />
         </Routes>
         <Footer />
       </BrowserRouter>
